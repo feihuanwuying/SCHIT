fee46209a411bd94de15600eab37a6bae92107ac
feihuanwuying <1397645429@qq.com>
2016-10-10 14:08:06 +0800

    创建user表对应的数据类

---
+++ src/Dao/User.java
@@ -0,0 +1,44 @@
+package Dao;
+
+/**
+ * 对应于数据表user
+ * Created by ZouKaifa on 2016/10/10.
+ */
+public class User {
+    private String username;  //用户名
+    private String password;  //密码
+    private String nickname;  //昵称
+    private String email;  //邮件
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
