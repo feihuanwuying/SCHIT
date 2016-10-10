1874ea2b370ffeb2a657334e0f698b61e9cf2160
feihuanwuying <1397645429@qq.com>
2016-10-10 15:24:07 +0800

    增加Dao.java，与数据库进行交互

---
+++ src/Dao/Dao.java
@@ -0,0 +1,21 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 用于获得数据库的连接
+ * Created by ZouKaifa on 2016/10/10.
+ */
+public class Dao {
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/SCHIT", "root", "960331");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
